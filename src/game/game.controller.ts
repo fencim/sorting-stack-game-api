1546e25daa0633b75818daf5732d914f6286a520
fencim <fencim@gmail.com>
2021-05-26 00:08:39 +0800

    add api to update game properties

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiBody, ApiOperation } from '@nestjs/swagger';
 import { Game } from 'src/interfaces/game.interface';
 import { GameService } from './game.service';
@@ -27,6 +27,29 @@ export class GameController {
     })
     @Get(':id')
     findOne(@Param('id') id: number): Game {
+        return this.gameService.findOne(id);
+    }
+    
+    @ApiOperation({
+        summary: "Update information of game with id {id}"
+    })
+    @ApiBody({
+        schema: {
+            type: "object",
+            properties: {
+                solved: {type: 'boolean', default: false},
+                popInfo: {
+                    type: 'object',
+                    properties: { 
+                        item: {type:'number', default: undefined}, 
+                        stack: {type:'number', default: undefined}
+                    }
+                }
+            }
+        }
+    })
+    @Put(':id')
+    update(@Param('id') id: number, @Body() game: Game): Game {
         return this.gameService.findOne(id);
     }
     @ApiOperation({
